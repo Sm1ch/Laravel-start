4af0902fd6b1dbb3884c32b47967ae2f3df3c769
Sm1ch <mguseynov8@gmail.com>
2023-01-31 22:52:07 +0300

    Продолжение изучение маршрутов

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -37,3 +37,32 @@ Route::get('/post/{id}', function ($id) {
 Route::get('/post/{name}/{surname}', function ($name, $surname) {
 		return $name . ' ' . $surname; // обрабатывает параметра входных данных имя и фамилии юзера
 });
+
+
+Route::get('/city/city/{city?}', function ($city = 'hasvegas') {
+	return 'Название города' . $city;                              // Необязательный параметры маршрута
+});
+
+
+Route::get('/posts/{id}', function ($id) {
+		return 'Проверка айди ' . $id;
+	})->where('id', '[0-9]+');                             // Ограниченные параметра маршрута
+
+   
+Route::get('/postik/{name}/{id}', function ($name, $id) {
+		return 'имя нефора ' . $name . ' ' . $id;
+	})->where('name', '[a-z0-9_-]+')->where('id', '[0-9]+');  // Ограничение нескольких параметров
+
+   
+Route::get('/post/{id}', function ($id) {
+		//
+	})->whereNumber('id');                  // Метод ограничывает параметры цифрами
+
+Route::get('/post/{name}', function ($name) {
+		
+	})->whereAlpha('name');                    // Метод ограничывает параметр только буквами
+
+Route::get('/post/{name}', function ($name) {
+		//
+	})->whereAlphaNumeric('name');             // Метод ограничывает параметры цифрами и буквами (Это были шаблонные ограничение)
+
