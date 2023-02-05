e4c88611866c99c1e0ef63b0dd1d12f801aae502
Sm1ch <mguseynov8@gmail.com>
2023-02-05 23:26:47 +0300

    изучал представление чуть-чуть коснулся blade

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -2,19 +2,33 @@
 	namespace App\Http\Controllers;
 	
 
+	// class PostController extends Controller
+	// {
+	// 	public function show($id)
+	// 	{	
+	// 		$users = [
+	// 		'user1' => 'Махачкала',
+	// 		'user2' => 'Кизилюрт',
+	// 		'user3' => 'Хасавюрт',
+	// 		'user4' => 'Кизляр',
+	// 		'user5' => 'Дербент',
+	// 	];
+	// 		return $users[$id];
+	// 	}
+	// }
+	
+	// class PostController extends Controller
+	// {
+	// 	public function show()
+	// 	{
+	// 		return view('test', ['name' => 'Къоз', 'surname' => 'Къозов']);
+	// 	}
+	// }
 	class PostController extends Controller
 	{
-		public function show($id)
-		{	
-			$users = [
-			'user1' => 'Махачкала',
-			'user2' => 'Кизилюрт',
-			'user3' => 'Хасавюрт',
-			'user4' => 'Кизляр',
-			'user5' => 'Дербент',
-		];
-			return $users[$id];
+		public function show()
+		{
+			return view('test', ['text' => 'Тут могла быть реклама легких колясок']);
 		}
 	}
-
 ?>
