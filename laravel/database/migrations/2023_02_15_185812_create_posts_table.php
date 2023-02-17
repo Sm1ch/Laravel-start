8c44f264cf99e983212a73bba088a08b99f0283a
Sm1ch <mguseynov8@gmail.com>
2023-02-17 21:37:28 +0300

    миграция

--- laravel/database/migrations/2023_02_15_185812_create_posts_table.php
+++ laravel/database/migrations/2023_02_15_185812_create_posts_table.php
@@ -15,7 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->text('text');
         });
     }
 
